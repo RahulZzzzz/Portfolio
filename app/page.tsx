802d9120defb5b2f24a29a5e250f7085e2bef4b9
Rahul Kumar <rjrahul1855@gmail.com>
2024-07-25 15:37:24 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -8,12 +11,15 @@ import { FaHome } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <main className="relative min-h-[100vh] bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center overflow-clip flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/> 
         <Hero />
         <RecentProjects/>
         <Grid/>
+        <Experience/>
+        <Approach/>
+        <Footer/>
       </div>
     </main>
   );
